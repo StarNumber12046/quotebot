27df6fe7236b84c783307f9262327ed2c914ec18
StarNumber12046 <starnumber.official12046@gmail.com>
2025-10-17 21:00:49 +0200

    Added quotes about me

--- apps/web/src/server/api/routers/quote.ts
+++ apps/web/src/server/api/routers/quote.ts
@@ -21,6 +21,41 @@ export const quotesRouter = createTRPCRouter({
       };
     }),
 
+  getQuotesAboutMe: publicProcedure.query(async ({ ctx }) => {
+    const [discordAccount] = await auth.api.listUserAccounts(ctx);
+    if (!discordAccount)
+      throw new TRPCError({
+        message: "This shouldn't happen :/",
+        code: "UNAUTHORIZED",
+      });
+    const res = await auth.api.getAccessToken({
+      body: {
+        providerId: "discord",
+      },
+      headers: ctx.headers,
+    });
+    console.log("res", res);
+
+    console.log("discordAccount", discordAccount);
+    const userQuotes = await ctx.db
+      .select()
+      .from(quotes)
+      .where(eq(quotes.authorId, discordAccount.accountId));
+
+    // Add cache details (users, channels, guilds) to the returned data for each quote
+    return Promise.all(
+      userQuotes.map(async (quote) => ({
+        ...quote,
+        author: (
+          await ctx.db
+            .select()
+            .from(usersCache)
+            .where(eq(usersCache.userId, quote.authorId))
+        )[0],
+      })),
+    );
+  }),
+
   getMyQuotes: publicProcedure.query(async ({ ctx }) => {
     const [discordAccount] = await auth.api.listUserAccounts(ctx);
     console.log("auth", discordAccount);
