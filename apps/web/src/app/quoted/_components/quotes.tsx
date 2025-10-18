87ee8248c0176e37d2c33f9c6436d183d46d4969
StarNumber12046 <starnumber.official12046@gmail.com>
2025-10-18 13:39:50 +0200

    Replaced button texts with icons

--- apps/web/src/app/quoted/_components/quotes.tsx
+++ apps/web/src/app/quoted/_components/quotes.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { api } from "~/trpc/react";
-import Image from "next/image";
 import { useEffect } from "react";
-import Link from "next/link";
+import { Quote } from "~/components/quote";
 
 export function Quotes() {
   const { data = [], isLoading } = api.quote.getQuotesAboutMe.useQuery();
@@ -23,19 +22,13 @@ export function Quotes() {
 
       <div className="flex flex-row flex-wrap justify-center gap-8">
         {data?.map((quote) => (
-          <div
+          <Quote
             key={quote.id}
-            className="flex flex-col items-center gap-2 rounded border-2 border-neutral-900 p-2"
-          >
-            <Link href={quote.imageStorageUrl}>
-              <Image
-                src={quote.imageStorageUrl}
-                alt={quote.quote}
-                width={400}
-                height={200}
-              />
-            </Link>
-          </div>
+            quote={quote}
+            canDelete={false}
+            canChangeVisibility={false}
+            canCopy={true}
+          />
         ))}
       </div>
     </div>
