ccd9dc6ac11c9d6e8951facaa0882f775c828156
StarNumber12046 <starnumber.official12046@gmail.com>
2026-01-06 20:04:46 +0100

    Fixed help command

--- apps/bot/src/commands/utility/help.ts
+++ apps/bot/src/commands/utility/help.ts
@@ -1,5 +1,5 @@
-import { ButtonBuilder, ButtonStyle, ComponentBuilder, SectionBuilder } from 'discord.js';
-import type { Command } from './index.js';
+import { ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentBuilder, SectionBuilder } from 'discord.js';
+import type { Command } from '../index.js';
 
 export default {
 	data: {
@@ -11,9 +11,9 @@ export default {
 			.setLabel('Visit Website')
 			.setURL('https://quotebot-web.vercel.app')
 			.setStyle(ButtonStyle.Link);
-		const section = new SectionBuilder().setButtonAccessory(button);
+		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
 		await interaction.reply({
-			components: [section],
+			components: [row],
 			content:
 				'Hey, I\'m a quotes bot. You can @mention me to quote a message or right click on one and use the "Quote Message" command.\nYou can also go to my website to see your quotes, quotes about you and more!',
 		});
